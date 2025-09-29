b79070f06bfdb2fbf8e1bb80a17ef9b28637dbb9
jadarcenko <162491049+GIGACHADTHEBEST@users.noreply.github.com>
2025-09-29 12:55:10 +0200

    Update checkandmaj.py

--- checkandmaj.py
+++ checkandmaj.py
@@ -1,2 +1,8 @@
-numbers_of_euros = 6767
-print (f" Vous avez actuellement {numbers_of_euros} € sur votre compte  ")
+def check money():
+    numbers_of_euros = 6767
+    print (f" Vous avez actuellement {numbers_of_euros} € sur votre compte  ")
+
+
+
+def maj_account():
+    numbers_of_euros = numbers_of_euros - money_taken 
