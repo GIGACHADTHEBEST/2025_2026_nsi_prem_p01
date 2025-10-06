2371fcb098307225379a0b7deee9d73911011a20
Styno144 <louiseltantawi@gmail.com>
2025-10-06 12:50:06 +0200

    Update menu.py

--- menu.py
+++ menu.py
@@ -6,15 +6,16 @@ def menu():
         print("2. Retirer de l'argent")
         print("3. Déposer de l'argent")
         print("4. Quitter")
-        choix = input("Votre choix : ")
+        choix = int(input("Votre choix : "))
         if choix == "1":
-            consulter_solde(solde)
+            check_money(solde)
         elif choix == "2":
-            solde = retirer_argent(solde)
+            solde = money_taken(solde)
         elif choix == "3":
-            solde = deposer_argent(solde)
+            solde = money_given(solde)
         elif choix == "4":
             print("Au revoir !")
             break
         else:
             print("Choix invalide.")
+
