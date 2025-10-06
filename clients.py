614a2c78e1f83e39812a2fcb4accb6941876f8e3
raphaelbtgm <raphael.btgm@gmail.com>
2025-10-06 12:35:48 +0200

    clients.py
    modification des clients

--- clients.py
+++ clients.py
@@ -1,5 +1,8 @@
 client = ["Nom":"Dupont", "Prénom":"Daniel", "ID": 1234, "Solde": 10000 [] ]
 
 
-client 1 = [Encoredeguèles, \ Jevés, \ 1243, \ 2000]
-    
+client_1 = [Encoredeguèles, \ Jevés, \ 1243, \ 2000]
+client_2 = [Horlayzom, \ Jade, \ 6666, \ 1500]
+client_3 = [Martin, \ Martin, \ 0000, \ 9005]
+client_4 = [Swamp, \ Iso, \ 9876, \ 10000]
+client_5 = [Adercenko, \ Victoriya, \ 1111, \ 3000]
