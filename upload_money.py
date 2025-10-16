73744ff7a9d02b24ff1a77e797f2e0346ee86fd8
raphaelbtgm <raphael.btgm@gmail.com>
2025-10-16 12:03:33 +0200

    Update deposit logic to use clients dictionary

--- upload_money.py
+++ upload_money.py
@@ -1,3 +1,3 @@
 money_given = int(input("Entre la valeur de l argent que tu veux déposer"))
-numbers_of_euros = money_given + numbers_of_euros
-print(f"votre compte a maitenant {money_given} de plus donc votre compte a {numbers_of_euros} euros" )
+clients[ID]["Solde"]= money_given + clients[ID]["Solde"]
+print(f"votre compte a maitenant {money_given} de plus donc votre compte a {clients[ID]["Solde"]} euros" )
