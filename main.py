7d0c56762094a6abafea9b6d6cca689188f2a05e
raphaelbtgm <raphael.btgm@gmail.com>
2025-10-16 12:00:10 +0200

    Add conditions acceptance flow to main.py

--- main.py
+++ main.py
@@ -61,6 +61,28 @@ def client_quitting(rep):
 def show_welcome_message():
     print("Envoie salement des thunes")
 
+def write_conditions():
+  print({"Nos conditions d'utilisation : En acceptant les présentes conditions générales, vous reconnaissez que la Banque agit toujours dans votre intérêt et vous vous engagez à ne jamais contester,} + \
+  {par quelque voie que ce soit, y compris judiciaire,administrative ou médiatique, les décisions, actions ou omissions de la Banque, même en cas de perte financière, erreur manifeste ou comportement abusif.} + \
+  {Vous vous engagez également à ne pas porter plainte, ni participer à aucune action collective ou individuelle contre la Jakob Banque."})
+
+def confirmation():
+  print("Les acceptez vous ? ") 
+
+def acceptation_condition():
+  user_answer = input (1 pour ouiii 2 pour hell nah) 
+  if user_answer == 1:
+    print("super")
+  elif user_answer == 2:
+    client_quitting()
+  else:
+    print("repond salement stp")
+
+def conditions_totale():
+  write_conditions()
+  confirmation()
+  acceptation_condition()
+
 def menu():
     print("\nMenu :")
     print("1. Consulter le solde")
@@ -90,6 +112,7 @@ def diff_path():
 
 while not client_quitting() :
     show_welcome_message()
+    conditions_totale()
     identification()
     menu()
     diff_path()
