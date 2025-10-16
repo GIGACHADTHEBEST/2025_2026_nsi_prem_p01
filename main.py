95960893f018e7bc362b4b59cc3cbee746cad004
raphaelbtgm <raphael.btgm@gmail.com>
2025-10-16 11:53:14 +0200

    Add client data structure in main.py

--- main.py
+++ main.py
@@ -1,5 +1,42 @@
 import json
 
+clients = {
+  1234 : [
+  "Nom":"Encoredeguèles",
+  "Prénom":"Jevés",
+  "Solde": 10000,
+  "Dépots": ["montant" : 563, "date": "2025-10-05"],
+  "Retraits": [] 
+  ],
+6767:[
+  "Nom":"Horlayzom",
+  "Prénom":"Jade",
+  "Solde": 1500,
+  "Dépots": ["montant" : 563, "date": "2025-10-05"],
+  "Retraits": [] 
+  ],
+0000:[
+  "Nom":"Horn",
+  "Prénom":"Gabe",
+  "Solde": 9005,
+  "Dépots": ["montant" : 563, "date": "2025-10-05"],
+  "Retraits": []
+  ],
+9876:[
+  "Nom":"Swamp",
+  "Prénom":"Iso",
+  "Solde": 10000,
+  "Dépots": ["montant" : 563, "date": "2025-10-05"],
+  "Retraits": []
+  ],
+5555:[
+  "Nom":"Hoho",
+  "Prénom":"Gotaga", 
+  "Solde": 10000,
+  "Dépots": ["montant" : 563, "date": "2025-10-05"],
+  "Retraits": []
+  ]
+}
 
 def read_clients_from_json_file(file):
     with open(file, 'r') as f:
